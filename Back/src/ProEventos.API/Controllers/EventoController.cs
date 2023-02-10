62761933a1c6e563e964bb03431a42712a646e92
Bruno Lima <brunomlima23@gmail.com>
2023-02-10 18:12:15 -0300

    Seção 2: DotNet 5

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+        public IEnumerable<Evento> _evento = new Evento[]{
+            new Evento(){
+                EventoId = 1,
+                Local = "Fortaleza",
+                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                Tema = ".Net e Angular",
+                QtdPessoas = 250,
+                Lote = "1",
+                ImagemURL = "foto.png"
+            }
+        };
+        
+        public EventoController()
+        {
+            
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _evento.Where(x => x.EventoId == id);
+        }
+    }
+}
